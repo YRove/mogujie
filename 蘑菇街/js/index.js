7489dd0ccf48e94a03182036c17e17b430926393
YRove <38689542+YRove@users.noreply.github.com>
2018-06-13 21:50:35 +0800

    YRove
    writing

---
+++ 蘑菇街/js/index.js
@@ -0,0 +1,113 @@
+var vm= new Vue({
+	el:'#show',
+	data:{
+	message:[
+	{'image':'img/nav1.png',
+	 'name':'女装'},
+	{'image':'img/nav2.png',
+	'name':'上衣'},
+	{'image':'img/nav3.png',
+	'name':'裤子'},
+	{'image':'img/nav4.png',
+	'name':'裙子'},
+	{'image':'img/nav5.png',
+	'name':'套装'}
+	]
+	}
+});
+new Vue({
+	el:'#show1',
+	data:{
+	message2:[
+	{'image':'img/nav6.png',
+	 'name':'内衣'},
+	{'image':'img/nav7.png',
+	'name':'美妆'},
+	{'image':'img/nav8.png',
+	'name':'女鞋'},
+	{'image':'img/nav9.png',
+	'name':'运动'},
+	{'image':'img/nav10.png',
+	'name':'包包'}
+	]
+	}
+});
+new Vue({
+	el:'#bar',
+	data:{
+	message:[
+	{'image':'img/590c3c426803b_11.png',
+	 'name':'首页'},
+	{'image':'img/590c3c426803b_11-13.png',
+	'name':'直播特卖'},
+	{'image':'img/590c3c26803b_11.png',
+	'name':'分类'},
+	{'image':'img/590c3c426803b_11-14.png',
+	'name':'购物车'},
+	{'image':'img/xddff.png',
+	'name':'我的'}
+	]
+	}
+})
+new Vue({
+	el:'#part2',
+	data:{
+		title:'穿搭课堂 - 春夏搭配指南',
+		title1:'1242004人在参与',
+			message:[
+	{'image':'img/class1.png',
+	 'name':'新品显瘦短袖'},
+	{'image':'img/class2.png',
+	'name':'连衣裙夏上新'},
+	{'image':'img/class3.png',
+	'name':'初夏衬衫套装'}
+	],
+	show:[
+	{'image':'img/c1.png',
+	 'name':'显瘦风',
+	 'name2':'兔斯基kayi'},
+	{'image':'img/c2.png',
+	'name':'T恤省心塔',
+	 'name2':'惠美'},
+	{'image':'img/c3.png',
+	'name':'冷淡风',
+	 'name2':'Halo'}
+	]
+	}
+});
+new Vue({
+	el:'#part3',
+	data:{
+		title:'今日必买',
+		title1:'BEST PICKS',
+	}
+});
+new Vue({
+	el:'#part4',
+	data:{
+		title:'流行元素 - 春夏上新',
+		title1:'NEW ARRIVAL',
+			message:[
+	{'image':'img/class1.png',
+	 'name':'新品显瘦短袖'},
+	{'image':'img/class2.png',
+	'name':'连衣裙夏上新'},
+	{'image':'img/class3.png',
+	'name':'初夏衬衫套装'}
+	],
+	show:[
+	{'image':'img/c1.png',
+	 'name':'显瘦风',
+	 'name2':'兔斯基kayi'},
+	{'image':'img/c2.png',
+	'name':'T恤省心塔',
+	 'name2':'惠美'},
+	{'image':'img/c3.png',
+	'name':'冷淡风',
+	 'name2':'Halo'}
+	]
+	}
+})
+
+
+
